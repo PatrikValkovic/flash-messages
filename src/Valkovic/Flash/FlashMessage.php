db5a474915df17929ee3cf90ced0a32a512d6498
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-02-20 14:49:11 +0100

    Created FlashMessage model

---
+++ src/Valkovic/Flash/FlashMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+namespace Valkovic\Flash;
+
+
+class FlashMessage
+{
+    /**
+     * @var string Text of message
+     */
+    public $text;
+
+    /**
+     * @var 'none'|'primary'|'info'|'success'|'warning'|'error' Type of message
+     */
+    public $type;
+
+    /**
+     * @var array Additional properties of message (id, class, etc)
+     */
+    public $properties;
+}
